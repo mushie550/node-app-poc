d9970092a8b2eca0b4cdb1766db76030589e5fe3
Musharraf Shaikh <89745515+mushie550@users.noreply.github.com>
2026-01-06 10:10:07 +0530

    Update pull_image.sh

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws ecr get-login-password --region me-central-1 | docker login --username AWS --password-stdin 767397902739.dkr.ecr.me-central-1.amazonaws.com
-docker pull 767397902739.dkr.ecr.me-central-1.amazonaws.com/node-docker-poc:latest
+sudo docker pull 767397902739.dkr.ecr.me-central-1.amazonaws.com/node-docker-poc:latest
 
